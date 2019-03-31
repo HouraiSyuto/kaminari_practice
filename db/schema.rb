7f948971024386c0b6a5ebbb631b35bb70936197
kenken <monasyu@pepabo.com>
2019-04-01 07:52:19 +0900

    画像アップロード機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_220605) do
+ActiveRecord::Schema.define(version: 2019_03_31_222835) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_220605) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
